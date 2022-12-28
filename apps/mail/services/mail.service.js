21c105bd23c3610b237c80c99e191d6a07e2ca72
ord669 <ord24@gmail.com>
2022-12-28 20:37:54 +0700

    added a little css to layout

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,13 +1,70 @@
-console.log('Hi')
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/storage.service.js'
+
+
+const MAILS_KEY = 'mailsDB'
 
 
 
-const email = {
-    id: 'e101', subject: 'Miss you!',
-    body: 'Would love to catch up sometimes',
-    isRead: false, sentAt: 1551133930594,
-    to: 'momo@momo.com'
+export const mailService = {
+    // query,
+    // get,
+    // remove,
+    // save,
+    getMails,
+    getLoggedinUser,
+    getCriteria,
+
 }
+
+
+
+
+
+const email = [
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+    {
+        id: utilService.makeId(), 
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false, sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    },
+]
 
 const loggedinUser = {
     email: 'user@appsus.com',
@@ -21,4 +78,17 @@ const criteria = {
     isRead: true, // (optional property, if missing: show all) 
     isStared: true, // (optional property, if missing: show all) 
     lables: ['important', 'romantic'] // has any of the labels 
+}
+
+
+function getMails() {
+    return email
+}
+
+function getLoggedinUser() {
+    return loggedinUser
+}
+
+function getCriteria() {
+    return criteria
 }
--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,10 +1,26 @@
+const { useState, useEffect } = React
+
+
 import { MailCompose } from "../cmps/mail-compose.jsx";
-import { MailDetails } from "./mail-details.jsx";
 import { MailFilter } from "../cmps/mail-filter.jsx";
 import { MailFolderList } from "../cmps/mail-folder-list.jsx";
 import { MailList } from "../cmps/mail-list.jsx";
+import { mailService } from "../services/mail.service.js";
 
 export function MailIndex() {
+
+    const [mails ,setMails] = useState(null)
+
+    useEffect(()=>{
+        loadMails()
+    },[])
+
+    function loadMails() {
+        
+        setMails(mailService.getMails()) 
+
+    }
+
     return <section className="mail-index">
         {/* mail-header */}
         {/* mail-side-bar */}
@@ -12,7 +28,9 @@ export function MailIndex() {
         <MailFilter />
         <MailFolderList />
         <MailCompose />
-        <MailList />
+
+        
+        {mails && <MailList mails={mails} />}
 
 
 
