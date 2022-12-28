d0e2d3780929182495203e8f26f2ba4b678f9833
ord669 <ord24@gmail.com>
2022-12-28 22:20:39 +0700

    added filter by txt

--- apps/mail/cmps/mail-filter.jsx
+++ apps/mail/cmps/mail-filter.jsx
@@ -1,7 +1,33 @@
+const { useState, useEffect } = React
 
-export  function MailFilter() {
-  return <section className="mail-filter">
-    mail-filter
-  </section>
+import { mailService } from "../services/mail.service.js"
+
+
+
+export function MailFilter({onSetFilter}) {
   
+  const [filterByToEdit, setFilterByToEdit] = useState(mailService.getDefaultFilter())
+
+  useEffect(()=>{
+    onSetFilter(filterByToEdit)
+  },[filterByToEdit])
+  
+
+  function handleChange({ target }) {
+    let { value, name: field, type } = target
+    value = (type === 'number') ? +value : value
+    setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+}
+
+  return <section className="mail-filter">
+
+    <input type="text"
+      name="txt"
+      placeholder="Search mail"
+      onChange={handleChange}
+      // ref={elInputRef}
+    />
+    
+  </section>
+
 }
--- services/async-storage.service.js
+++ services/async-storage.service.js
@@ -4,6 +4,8 @@ export const storageService = {
     post,
     put,
     remove,
+    _save,
+    _makeId,
 }
 
 function query(entityType, delay = 500) {
