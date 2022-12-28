4b0a7249e5d4535c6a1da56d4cc53521a63a66f6
BatelKat96 <batelkat@gmail.com>
2022-12-28 19:00:45 +0200

    start build crudl note

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,58 +1,84 @@
-// import { storageService } from '../../../services/storage.service.js'
-// import { storageService } from '../../../services/storage.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
 
 const NOTES_KEY = 'notesDB'
 
+_createNotes()
 
 export const noteService = {
-    getNotes
+    query,
+    save,
+    getEmptyNote
 }
 
 
-// function query() {
+function query() {
+    return storageService.query(NOTES_KEY)
+}
 
-//     return storageService.query(NOTES_KEY)
+function save(note) {
+    console.log('he:')
 
-// }
+    if (note.id) {
+        return storageService.put(NOTES_KEY, note)
+    } else {
+        return storageService.post(NOTES_KEY, note)
+    }
+}
 
-
-
-
-function getNotes() {
-    console.log('hi:')
-    return [
-        {
-            id: "n101",
-            type: "note-txt",
-            isPinned: true,
-            info: {
-                txt: "Fullstack Me Baby!"
-            }
-        },
-        {
-            id: "n102",
-            type: "note-img",
-            info: {
-                url: "../../../assets/img/audi.jpg",
-                title: "Bobi and Me"
-            },
-            style: {
-                backgroundColor: "#00d"
-            }
-        },
-        {
-            id: "n103",
-            type: "note-todos",
-            info: {
-                label: "Get my stuff together",
-                todos: [
-                    { txt: "Driving liscence", doneAt: null, id: 100 },
-                    { txt: "Coding power", doneAt: 187111111, id: 101 }
-                ]
-            }
+function getEmptyNote(type = 'note-txt', txt = '') {
+    return {
+        type,
+        isPinned: false,
+        info: {
+            txt,
         }
-    ]
+    }
 }
+
+
+
+function _createNotes() {
+    console.log('hi:')
+
+    let notes = query()
+    if (!notes || !notes.length) {
+        notes = [
+            {
+                id: "n101",
+                type: "note-txt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!"
+                }
+            },
+            {
+                id: "n102",
+                type: "note-img",
+                info: {
+                    url: "../../../assets/img/audi.jpg",
+                    title: "Bobi and Me"
+                },
+                style: {
+                    backgroundColor: "#00d"
+                }
+            },
+            {
+                id: "n103",
+                type: "note-todos",
+                info: {
+                    label: "Get my stuff together",
+                    todos: [
+                        { txt: "Driving liscence", doneAt: null, id: storageService._makeId() },
+                        { txt: "Coding power", doneAt: 187111111, id: storageService._makeId() }
+                    ]
+                }
+            }
+        ]
+    }
+    storageService._save(NOTES_KEY, notes)
+}
+
 
 
 // const notes = [
