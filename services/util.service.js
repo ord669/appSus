6a7e7bdd00374b139a9055fd66931a8f7ad20217
ord669 <ord24@gmail.com>
2022-12-29 00:03:13 +0700

    added load

--- services/util.service.js
+++ services/util.service.js
@@ -6,6 +6,9 @@ export const utilService = {
     padNum,
     getDayName,
     getMonthName,
+    saveToStorage,
+    loadFromStorage,
+    
 }
 
 function makeId(length = 6) {
@@ -59,4 +62,14 @@ function getMonthName(date) {
         "July", "August", "September", "October", "November", "December"
     ]
     return monthNames[date.getMonth()]
+}
+
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+    const data = localStorage.getItem(key)
+    return (data) ? JSON.parse(data) : undefined
 }
