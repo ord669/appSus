32c07a00662a66a8620f3da5f277e32ae52cbdd9
BatelKat96 <batelkat@gmail.com>
2022-12-31 18:44:09 +0200

    Home page basic

--- views/home.jsx
+++ views/home.jsx
@@ -1,9 +1,16 @@
 import { Hero } from "../cmps/hero.jsx";
+const { Link, NavLink, useLocation } = ReactRouterDOM
 
 export function Home() {
 
     return <section className="home">
-        <Hero />
-        <h1>Welcome to home page!</h1>
+        {/* <Hero /> */}
+        <h1>AppsSus</h1>
+
+        <div className="main-home">
+            <Link to="/about" className="home-img home-img-about "> <img src="./assets/img/poem.png" alt="Two pepole holding poem" title="About" /> </Link>
+            <Link to="/mail/inbox" className="home-img home-img-mail"><img src="./assets/img/gmail.png" alt="mail logo" title="Mail" /></Link>
+            <Link to="/note" className="home-img home-img-note" ><img src="./assets/img/note.png" alt="note logo" title="Note" /></Link>
+        </div>
     </section>
 }
