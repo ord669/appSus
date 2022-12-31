0f56d69fb0db12baab4252fdb8ef2f07b95d9a56
BatelKat96 <batelkat@gmail.com>
2022-12-31 20:54:21 +0200

    submission 21:00

---
+++ cmps/hamburger.jsx
@@ -0,0 +1,11 @@
+export function Hamburger() {
+
+
+    return <div className="hamburger-page">☰
+        <div className="burger burger1" />
+        <div className="burger burger2" />
+        <div className="burger burger3" />
+    </div>
+
+
+}
